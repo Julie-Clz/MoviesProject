31acbd94d6e6f85761173d5062bec1a77792b58b
julie collazos <juliecollazos@hotmail.com>
2022-02-16 21:05:45 +0100

    Initial Commit

---
+++ MoviesProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesProject
+//
+//  Created by Julie Collazos on 16/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoviesProject/MoviesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesProjectApp.swift
+//  MoviesProject
+//
+//  Created by Julie Collazos on 16/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
